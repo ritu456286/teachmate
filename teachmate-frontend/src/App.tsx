6ee13f0ad4f1986c12ed373c57191681dac124da
RITU KANSAL <87207504+ritu456286@users.noreply.github.com>
2025-02-12 16:27:05 +0530

    Added themes-dark and light

--- teachmate-frontend/src/App.tsx
+++ teachmate-frontend/src/App.tsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeProvider } from "@/components/theme-provider"
+import { Routes, Route } from "react-router";
+import LandingPage from './pages/LandingPage';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <> <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    {
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+      </Routes>
     
+    }
+  </ThemeProvider>
+      
     </>
   )
 }
---
+++ teachmate-frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from '@/components/mode-toggle'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <ModeToggle/>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default LandingPage
